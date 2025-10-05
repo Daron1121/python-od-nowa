dc364934ab0f452bd67a4e95cdf80af73db2f8cf
Daron1121 <168169947+Daron1121@users.noreply.github.com>
2025-10-05 21:04:55 +0200

    Lvl 1 i Lvl 2 z modules - do dokonczenia lvl 3

--- day12_modules/Main.py
+++ day12_modules/Main.py
@@ -31,8 +31,76 @@ print(math.log10(100))   # 2, logarithm with 10 as base
 import string
 print(string.ascii_letters) # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 print(string.digits)        # 0123456789
-print(string.punctuation)   # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+print(string.punctuation)   # "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 
-from random import random, randint
+from random import *
 print(random())   # it doesn't take any arguments; it returns a value between 0 and 0.9999
 print(randint(5, 20)) # it returns a random integer number between [5, 20] inclusive
+
+#---------------------------------------------------------------------------------
+#* Level 1 
+print(50 * '-')
+#Ex 1
+def random_user_id(): 
+    to_make = string.ascii_letters + string.digits
+    id = ''
+    for iter in range(8):
+        id += choice(to_make)
+    return f'Your password is : {id}'
+print(random_user_id())
+
+#Ex 2
+def user_id_gen_by_user(): 
+    num_of_chars = input("Input How many characters your password should have: ")
+    num_of_IDs = input("Input How many IDs you want: ")
+    to_make = string.ascii_letters + string.digits
+    for passwords in range(int(num_of_IDs)):
+        id = ''
+        for iter in range(int(num_of_chars)):
+            id += choice(to_make)
+
+        print(id)
+#print(user_id_gen_by_user())
+
+#Ex 3
+def rgb_color_gen():
+    rgb = []
+    for iter in range(3):
+        rgb.append(randint(000,255))
+    return f'RGB{rgb}'
+print(rgb_color_gen())
+
+#* Level 2
+#Ex 1
+def list_of_hexa_colors(how_many_colors):
+    to_make = 'ABCDEF' + string.digits
+    hexa = []
+    for iter in range(how_many_colors):
+        iterated_color = ''
+        for number in range(6):
+            iterated_color += choice(to_make)
+        hexa.append(f'#{iterated_color}')
+    return hexa
+print(list_of_hexa_colors(3))
+
+#Ex 2
+def list_of_rgb_colors(how_many_colors):
+    rgb = []
+    for iter in range(how_many_colors):
+        iterated_color = []
+        for iter in range(3):
+            iterated_color.append(randint(000,255))
+        rgb.append(f'rgb{iterated_color}')
+    return rgb
+print(list_of_rgb_colors(2))
+
+#Ex 3
+def generate_colors(h_or_rgb,how_many_colors):
+    if h_or_rgb == 'rgb':
+        return list_of_rgb_colors(how_many_colors)
+    else:
+        return list_of_hexa_colors(how_many_colors)
+print(generate_colors('hexa', 3)) # ['#a3e12f','#03ed55','#eb3d2b'] 
+print(generate_colors('hexa', 1)) # ['#b334ef']
+print(generate_colors('rgb', 3))  # ['rgb(5, 55, 175','rgb(50, 105, 100','rgb(15, 26, 80'] 
+print(generate_colors('rgb', 1))  # ['rgb(33,79, 176)']
