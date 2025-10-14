1725a498424f88f6298c8fa8175acde7beb4adea
Daron1121 <168169947+Daron1121@users.noreply.github.com>
2025-10-14 18:53:19 +0200

    no exercises done

---
+++ day15_type_errors/type_errors.py
@@ -0,0 +1,14 @@
+'''
+Most common errors: 
+SyntaxError
+NameError
+IndexError
+ModuleNotFoundError
+AttributeError
+KeyError
+TypeError
+ImportError
+ValueError
+ZeroDivisionError
+Rest in documentation.
+'''
