6b9d89ac1b1178b6895ce1b7888f221cd3cb0f6d
Daron1121 <168169947+Daron1121@users.noreply.github.com>
2025-11-05 21:26:36 +0100

    Dokoczenie zadan z package menagera (requests)

--- 30DaysOfPython/day20_Python_package_manager/Pip.py
+++ 30DaysOfPython/day20_Python_package_manager/Pip.py
@@ -35,32 +35,107 @@ cats_api = 'https://api.thecatapi.com/v1/breeds'
 # the min, max, mean, median, standard deviation of cats' weight in metric units.
 cats = requests.get(cats_api)
 kitten_info = cats.json()
-metrics = []
+information = []
 for cat in kitten_info:
-    metrics.append(cat['weight']['metric'])
-nowe = []
-for met in metrics:
-    met = met.split(" - ")
-    liczby = [float(y) for y in met]
-    srednia = sum(liczby)/len(liczby)
-    nowe.append(srednia)
-print(min(nowe))
-print(max(nowe))
-print(statistics.mean(nowe))
-print(statistics.median(nowe))
-print(statistics.stdev(nowe))
+    information.append(cat['weight']['metric'])
 
+
+def for_cats(to_iterate):
+    nowe = []
+    for met in to_iterate:
+        met = met.split(" - ")
+        liczby = [float(y) for y in met]
+        srednia = sum(liczby)/len(liczby)
+        nowe.append(srednia)
+    print(min(nowe))
+    print(max(nowe))
+    print(statistics.mean(nowe))
+    print(statistics.median(nowe))
+    print(statistics.stdev(nowe))
+# the min, max, mean, median, standard deviation of cats' weight in metric units.
+cats = requests.get(cats_api)
+kitten_info = cats.json()
+information = []
+for cat in kitten_info:
+    information.append(cat['weight']['metric'])
+print(for_cats(information))
+
+print(50*'-')
 # the min, max, mean, median, standard deviation of cats' lifespan in years.
+information = []
+for cat in kitten_info:
+    information.append(cat['life_span'])
+print(for_cats(information))
+
 
 # Create a frequency table of country and breed of cats
-
+def frequency(of_what):
+    information = []
+    for cat in kitten_info:
+        information.append(cat[of_what])
+    freq = {}
+    for info in information:
+        if info in freq:
+            freq[info] += 1
+        else:
+            freq[info] = 1
+    return freq
+print(frequency('origin'))
+print(frequency('name'))
 
 
 # Ex 3 Read the countries API and find
-# the 10 largest countries
-# the 10 most spoken languages
-# the total number of languages in the countries API
+url = "https://restcountries.com/v3.1/all?fields=name,capital,region,population,languages"
+response = requests.get(url)
+countries = response.json()
+print(json.dumps(countries, indent=2, ensure_ascii=False))
 
+#  Pomocnicza funkcja
+def get_info(field):
+    info = {}
+    for country in countries:
+        name = country["name"]["common"]
+        value = country.get(field, None)
+        info[name] = value
+    return info
+
+def find_it(smth):
+    info = get_info(smth)
+    if smth == "languages":
+        # traktuj to jako języki
+        lang_count = {}
+        for langs in info.values():
+            if isinstance(langs, dict):
+                for lang in langs.values():
+                    if lang in lang_count:
+                        lang_count[lang] += 1
+                    else:
+                        lang_count[lang] = 1
+        sorted_arr = sorted(lang_count.items(), key= lambda x : x[1], reverse=True)
+        return sorted_arr[:10]
+    elif smth == "population":
+        # traktuj to jako liczby (np. populacja)
+        sorted_arr = sorted(info.items(), key= lambda x : x[1], reverse=True)
+        return sorted_arr[:10]
+    else: 
+        pass
+
+
+# the 10 most populated
+print(find_it('population'))
+
+# the 10 most spoken languages
+print(find_it('languages'))
+
+# the total number of languages in the countries API
+def lang_count(smth):
+    info = get_info(smth)
+    unique_langs = set()
+    for langs in info.values():
+        for lang in langs.values():
+            unique_langs.add(lang)
+    return f'Total number of unique countries in API: {len(unique_langs)}'
+print(lang_count('languages')) 
 
 
 # Ex 4 UCI is one of the most common places to get data sets for data science and machine learning. Read the content of UCL (https://archive.ics.uci.edu/ml/datasets.php). Without additional libraries it will be difficult, so you may try it with BeautifulSoup4
